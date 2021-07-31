af9e5ca9cfd22ed563295e979d252d603d1b0dd0
Alan-Lomax <87946633+Alan-Lomax@users.noreply.github.com>
2021-07-30 21:39:20 -0400

    Made all variables consistent unsigned long

--- examples/Timer_Example/Timer.h
+++ examples/Timer_Example/Timer.h
@@ -17,26 +17,26 @@
 */
 class Timer {
   private:
-    long _alarmTime;                    // milliseconds before sounding alarm
-    long _pauseTime;                    // milliseconds spent in pause
-    long _runTime;                      // running time in milliseconds (time that timer has been running after deducting _pauseTime)
-    bool _pause;                        // true if we are in pause false if not
-    bool _running;                      // true if timer is running, false if not
-    bool _alarm;                        // true if timer has expired (In alarm condition), false if not
+    unsigned long _alarmTime;           // milliseconds before sounding alarm
+    unsigned long _pauseTime;           // milliseconds spent in pause
+    unsigned long _runTime;             // running time in milliseconds (time that timer has been running after deducting _pauseTime)
     unsigned long _previousMillis;      // will store the start time of the timer
-    void init();                        // (re) Initialize the timer
+    bool _pause;                        // only true if we are paused
+    bool _running;                      // only true if timer is running
+    bool _alarm;                        // only true if timer has expired (We are in an alarm condition)
+    void init();                        // Initialize or reset the timer the timer
 
   public:
-    Timer(long MyalarmTime);            // Default definition specifyng the alarm time in milliseconds. Timer will be in stopped state, running time will not be accumulate until started.
+    Timer(unsigned long MyalarmTime);   // Default definition specifyng the alarm time in milliseconds. Timer will be in stopped state, running time will not be accumulate until started.
     void update();                      // update things based on elapsed time (call this as often as possible)
     void start();                       // Start the Timer by setting _running to true and zeroing timing values. If we were paused - unpause us but do not change timing values.
     void stop();                        // Stop the Timer by setting _running to false (And reset _runTime and _pauseTime to zero)
     void pause();                       // Toggle the Timer from _paused = true to _paused=false (When _paused = true accumulate time in _pausetime which is to be deducted from running time.)
     bool alarm();                       // Returns true if elapsed time - _pausetime has exceeded the alarmTime, false otherwise.
-    long currentValue();                // Returns the current value of the timer (in millis)
-    long pauseTime();                   // Returns the current value of the Pause timer (in millis)
-    long alarmTime();                   // Returns the current setting of the Alarm Timer (in millis)
-    void setalarmTime(long MyalarmTime);// Resets the Alarm Timer to a new value(in millis)
-    bool running();                     // return true / false according to if timer is runnning.
+    unsigned long currentValue();                // Returns the current value of the timer (in millis)
+    unsigned long pauseTime();                   // Returns the current value of the Pause timer (in millis)
+    unsigned long alarmTime();                   // Returns the current setting of the Alarm Timer (in millis)
+    void setalarmTime(unsigned long MyalarmTime);// Resets the Alarm Timer to a new value(in millis)
+    bool running();                              // return true / false according to if timer is runnning.
 };
 #endif
--- examples/Timer_Example/Timer_Example.ino
+++ examples/Timer_Example/Timer_Example.ino
@@ -4,35 +4,35 @@
 #include "Timer.h"
 const byte myLed = LED_BUILTIN;       // We will use on board LED to idicate what is going on
 
-// Notice in the line below the OnTime or OffTime is not defined (they could have been). Instead I set them separately on lines below.
-Timer myTimer1(5000);                // define myTimer1 as a member of the Timer class and set to 10 seconds.
-Timer myTimer2(1000);                 // define myTimer2 and set to 1 second
+// Define two members of the Timer class
+Timer myTimer1(5000);                 // myTimer1 is set to 5 seconds.
+Timer myTimer2(1000);                 // myTimer2 is set to 1 second
 
 
 void setup() {
-  Serial.begin(9600);                 // for serial / debug console
+  Serial.begin(115200);               // for serial / debug console
   Serial.println(__FILE__);           // by default I like to output the file name as a minimum.
   pinMode(LED_BUILTIN, OUTPUT);       // define our builtin LED as an output pin
   myTimer1.start();                   // start the 10 second timer right away
 }
 
-// In the loop the update function runs over and over again - it does all the work.
-
 void loop() {
+  // The update function does all the hard work - but we dont need to see it happening.
   myTimer1.update();                     // update timing values
   myTimer2.update();                     // update timing values
 
-  if (myTimer1.alarm() == true) {        // If 10 seconds are up
+  // The rest of this sample program is inspecting properties of our timers and starting or stopping them.
+  if (myTimer1.alarm() == true) {        // If time is up
     Serial.println("Timer1 in alarm.");
     digitalWrite (LED_BUILTIN, true);    // Turn on the LED
-    myTimer1.stop();                     // stop the 10 second timer
-    myTimer2.start();                    // and start the 1 second timer
+    myTimer1.stop();                     // stop the timer
+    myTimer2.start();                    // and start the second timer
   }
-  if (myTimer2.alarm() == true) {      // If the extra 1 second is up
+  if (myTimer2.alarm() == true) {        // If the extra time is up
     Serial.println("  >> Timer2 in alarm.");
     digitalWrite (LED_BUILTIN, false); // Turn off the LED
 
-    myTimer2.stop();                   // stop the 1 second timer (which is just in alarm now)
-    myTimer1.start();                  // And restart the 10 second timer
+    myTimer1.start();                  // Restart the first timer
+    myTimer2.stop();                   // and stop the second timer
   }
 }
